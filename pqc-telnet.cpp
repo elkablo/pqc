db848ce5c6a65fc87e00927d443b41bae349df1d
Marek Behún <kabel@blackhole.sk>
2016-12-22 02:33:46 +0100

    Remove unnecessary includes

--- pqc-telnet.cpp
+++ pqc-telnet.cpp
@@ -15,10 +15,6 @@
 #include <termios.h>
 #include <pqc_socket_session.hpp>
 
-#include <pqc_sidh_key.hpp>
-#include <pqc_auth.hpp>
-#include <pqc_base64.hpp>
-
 using namespace std;
 using namespace pqc;
 
